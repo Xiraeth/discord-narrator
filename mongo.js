220cfc1e0a0fff5aa201a5f81fc68c79a2d427c2
nick <nickiliadis99@gmail.com>
2026-01-25 18:21:48 +0200

    separate mongo logic
    database connection from schema creation

--- mongo.js
+++ mongo.js
@@ -11,35 +11,4 @@ const connectToMongo = () =>
       console.error("Error connecting to MongoDB database", err);
     });
 
-const UserGuildSchema = new mongoose.Schema({
-  guildId: String,
-  nickname: String,
-  roles: [String],
-  warnings: Number,
-  messagesTotal: Number,
-  botInteractions: Number,
-  joinedAt: Number, // timestamp
-});
-
-const MessageSchema = new mongoose.Schema({
-  userId: String,
-  messageId: String,
-  channelId: String,
-  guildId: String,
-  timestamp: Date,
-});
-
-const UserSchema = new mongoose.Schema({
-  userId: String,
-  username: String,
-  globalName: String,
-  avatar: String,
-  bot: Boolean,
-  createdAt: Number, // timestamp
-  guilds: [UserGuildSchema],
-});
-
-const Message = mongoose.model("Message", MessageSchema);
-const User = mongoose.model("User", UserSchema);
-
-module.exports = { connectToMongo, Message, User };
+module.exports = { connectToMongo };
